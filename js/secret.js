b07cf67f7765cdfd70c08f108b72b018c33b32b8
agentisnotreal <55689844+agentisnotreal@users.noreply.github.com>
2024-08-15 00:51:38 +1000

    Change fonts and icons (whoops)

--- js/secret.js
+++ js/secret.js
@@ -1,29 +1,52 @@
 let activated = false;
 
-const konamiSequence = ['ArrowUp', 'ArrowUp', 'ArrowDown', 'ArrowDown', 'ArrowLeft', 'ArrowRight', 'ArrowLeft', 'ArrowRight', 'b', 'a'];
+const konamiSequence = [
+  "ArrowUp",
+  "ArrowUp",
+  "ArrowDown",
+  "ArrowDown",
+  "ArrowLeft",
+  "ArrowRight",
+  "ArrowLeft",
+  "ArrowRight",
+  "b",
+  "a",
+];
+
+const symbolRep = {
+  ArrowUp: "↑",
+  ArrowDown: "↓",
+  ArrowLeft: "←",
+  ArrowRight: "→",
+  b: "B",
+  a: "A",
+};
+
 let keypresses = [];
 
-document.getElementById('secret').style = 'visibility: visible;';
+document.getElementById("secret").style = "visibility: visible;";
 
-document.addEventListener('keydown', event => {
-    if (!konamiSequence.includes(event.key)) return;
+document.addEventListener("keydown", (event) => {
+  if (!konamiSequence.includes(event.key)) return;
 
-    if (activated === false) {
-        setTimeout(() => {
-            keypresses = [];
-            activated = false;
-        }, 3000);
-        activated = true;
-    }
+  if (activated === false) {
+    setTimeout(() => {
+      keypresses = [];
+      activated = false;
+    }, 3000);
+    activated = true;
+  }
 
-    keypresses.push(event.key);
-    if (keypresses[keypresses.length - 1] != konamiSequence[keypresses.length - 1]) {
-        keypresses = [];
-        activated = false;
-    }
+  keypresses.push(event.key);
+  if (
+    keypresses[keypresses.length - 1] != konamiSequence[keypresses.length - 1]
+  ) {
+    keypresses = [];
+    activated = false;
+  }
 
-    if (keypresses.toString() == konamiSequence.toString()) {
-        window.location = 'https:/\/www.youtube.com/watch?v=oHg5SJYRHA0';
-        activated = false;
-    }
+  if (keypresses.toString() == konamiSequence.toString()) {
+    window.location = "https://www.youtube.com/watch?v=oHg5SJYRHA0";
+    activated = false;
+  }
 });
